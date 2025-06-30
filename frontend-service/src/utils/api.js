e6ff65982006cc691c8d63e759b448b8b264dcda
nhuttmse172865 <nhuttmse172865@fpt.edu.vn>
2025-06-30 15:14:54 +0700

    fix communicaate

--- frontend-service/src/utils/api.js
+++ frontend-service/src/utils/api.js
@@ -7,7 +7,7 @@ const api = axios.create({
 
 export const login = (credentials) =>
   axios.post(
-    'http://gateway-service/account-service/api/users/login', credentials,
+    'http://gateway-service:8080/account-service/api/users/login', credentials,
     {
       headers: {
         'Content-Type': 'application/json',
@@ -19,7 +19,7 @@ export const logout = () =>
 
 // Movie Management APIs
 export const getAllMovies = (params = {}) =>
-  api.get('http://gateway-service/cinema-service/movies', { params },
+  api.get('http://gateway-service:8080/cinema-service/movies', { params },
     {
       headers: {
         'Content-Type': 'application/json',
@@ -28,79 +28,79 @@ export const getAllMovies = (params = {}) =>
   );
 
 export const getMovieById = (id) =>
-  api.get(`http://gateway-service/cinema-service/movies/${id}`);
+  api.get(`http://gateway-service:8080/cinema-service/movies/${id}`);
 
 export const createMovie = (movieData) =>
-  api.post('http://gateway-service/cinema-service/movies/create', movieData);
+  api.post('http://gateway-service:8080/cinema-service/movies/create', movieData);
 
 export const updateMovie = (id, movieData) =>
-  api.put(`http://gateway-service/cinema-service/movies/update/${id}`, movieData);
+  api.put(`http://gateway-service:8080/cinema-service/movies/update/${id}`, movieData);
 
 export const deleteMovie = (id) =>
-  api.delete(`http://gateway-service/cinema-service/movies/delete/${id}`);
+  api.delete(`http://gateway-service:8080/cinema-service/movies/delete/${id}`);
 
 export const getShowtimesByMovie = (movieId) =>
-  api.get(`http://gateway-service/cinema-service/movies/${movieId}/showtimes`);
+  api.get(`http://gateway-service:8080/cinema-service/movies/${movieId}/showtimes`);
 
 export const getMovies = (params = {}) =>
-  api.get('http://gateway-service/cinema-service/movies', { params }, {
+  api.get('http://gateway-service:8080/cinema-service/movies', { params }, {
     headers: {
       'Content-Type': 'application/json',
     },
   });
 
 export const getShowtimesByMovieDate = (movieId, date) =>
-  api.get(`http://gateway-service/cinema-service/showtimes/movie/show-time-date/${movieId}`, { params: { date } }, {
+  api.get(`http://gateway-service:8080/cinema-service/showtimes/movie/show-time-date/${movieId}`, { params: { date } }, {
     headers: {
       'Content-Type': 'application/json',
     },
   });
 
 export const getShowtimeById = (showtimeId) =>
-  api.get(`http://gateway-service/cinema-service/showtimes/${showtimeId}`);
+  api.get(`http://gateway-service:8080/cinema-service/showtimes/${showtimeId}`);
 
 export const getMovieDetail = (movieId) =>
-  api.get(`http://gateway-service/cinema-service/movies/${movieId}`);
+  api.get(`http://gateway-service:8080/cinema-service/movies/${movieId}`);
 
 export const getConcessionProducts = (params = {}) =>
-  api.get('http://gateway-service/cinema-service/concession-products', { params });
+  api.get('http://gateway-service:8080/cinema-service/concession-products', { params });
 
 export const getConcessionProductDetail = (productId) =>
-  api.get(`http://gateway-service/cinema-service/concession-products/${productId}`);
+  api.get(`http://gateway-service:8080/cinema-service/concession-products/${productId}`);
 
 
 
 // Concession Product APIs
 export const getAllProducts = (params = {}) =>
-  api.get('http://gateway-service/cinema-service/concession-products', { params });
+  api.get('http://gateway-service:8080/cinema-service/concession-products', { params });
 
 export const getProductById = (id) =>
-  api.get(`http://gateway-service/cinema-service/concession-products/${id}`);
+  api.get(`http://gateway-service:8080/cinema-service/concession-products/${id}`);
 
 export const createProduct = (data) =>
-  api.post('http://gateway-service/cinema-service/concession-products', data);
+  api.post('http://gateway-service:8080/cinema-service/concession-products', data);
 
 export const updateProduct = (id, data) =>
-  api.put(`http://gateway-service/cinema-service/concession-products/${id}`, data);
+  api.put(`http://gateway-service:8080/cinema-service/concession-products/${id}`, data);
 
 export const deleteProduct = (id) =>
-  api.delete(`http://gateway-service/cinema-service/concession-products/${id}`);
+  api.delete(`http://gateway-service:8080/cinema-service/concession-products/${id}`);
 
 export const createBooking = (bookingData) =>
-  api.post('http://gateway-service/booking-service/api/booking', bookingData);
+  api.post('http://gateway-service:8080/booking-service/api/booking', bookingData);
 
 export const getShowtimeTicketPrice = (showtimeId, ticketType) =>
-  api.get(`http://gateway-service/cinema-service/showtime-ticket-prices/showtime/${showtimeId}/ticket-type/${ticketType}`);
+  api.get(`http://gateway-service:8080/cinema-service/showtime-ticket-prices/showtime/${showtimeId}/ticket-type/${ticketType}`);
 
 export const createPayment = (bookingId, payment) =>
-  api.post(`http://gateway-service/booking-service/api/payment/create?bookingId=${bookingId}`, payment);
+  api.post(`http://gateway-service:8080/booking-service/api/payment/create?bookingId=${bookingId}`, payment);
 
 export const getSeatsByShowtime = (showtimeId) =>
-  api.get(`http://gateway-service/booking-service/showtimes/${showtimeId}/seats/unavailable`);
+  api.get(`http://gateway-service:8080/booking-service/showtimes/${showtimeId}/seats/unavailable`);
 
 export const checkStudentDiscount = (image) =>
-  api.post(`http://gateway-service/booking-service/ai/vertex/check-image`, image);
+  api.post(`http://gateway-service:8080/booking-service/ai/vertex/check-image`, image);
 
 export const getHistoryBooking = (userId) =>
-  api.get(`http://gateway-service/booking-service/api/booking/customer/${userId}`);
+  api.get(`http://gateway-service:8080/booking-service/api/booking/customer/${userId}`);
 
