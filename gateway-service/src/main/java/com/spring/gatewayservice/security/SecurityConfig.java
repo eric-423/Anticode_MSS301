5dea6e0f33eb36156251d96df5135f1931a0f57f
nhuttmse172865 <nhuttmse172865@fpt.edu.vn>
2025-07-21 23:16:52 +0700

    done config

--- gateway-service/src/main/java/com/spring/gatewayservice/security/SecurityConfig.java
+++ gateway-service/src/main/java/com/spring/gatewayservice/security/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://34.126.66.29"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://35.240.150.111"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
