f53d384dea37aab75ca886f583cdcc85ee92f146
Gatsby204 <Hieuptse172845@fpt.edu.vn>
2025-07-24 23:37:02 +0700

    Validate showtime

--- cinema-service/src/main/java/com/example/cinemaservice/repository/ShowtimeRepository.java
+++ cinema-service/src/main/java/com/example/cinemaservice/repository/ShowtimeRepository.java
@@ -1,9 +1,13 @@
 package com.example.cinemaservice.repository;
 
+import com.example.cinemaservice.entity.CinemaHall;
 import com.example.cinemaservice.entity.Showtime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -16,4 +20,9 @@ public interface ShowtimeRepository extends JpaRepository<Showtime, Integer> {
     Showtime getShowtimesById(int id);
 
     List<Showtime> getShowtimesByCinemaHall_Id(int cinemaHallId);
+
+    List<Showtime> findByCinemaHallAndStartTimeBeforeAndEndTimeAfter(CinemaHall cinemaHall, Date endTime, Date startTime);
+
+    @Query("SELECT s FROM Showtime s WHERE s.cinemaHall.id = :cinemaHallId AND s.startTime < :endTime AND s.endTime > :startTime")
+    List<Showtime> findConflictingShowtimes(@Param("cinemaHallId") Integer cinemaHallId, @Param("startTime") Date startTime, @Param("endTime") Date endTime);
 }
